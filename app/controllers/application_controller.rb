8b5bc7dfce1f04412e818bfc40143ff7ba385cbb
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-01-14 14:10:26 -0500

    Add bio to strong params permitted list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permited_parameters, if: :devise_controller?
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:bio, :name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
